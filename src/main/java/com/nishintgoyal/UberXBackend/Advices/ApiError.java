86d7bb960ec3d575d196ec95495f828495375490
nishint0001 <nishintgoyal0001@gmail.com>
2025-03-19 23:45:57 +0530

    Added Advices

---
+++ src/main/java/com/nishintgoyal/UberXBackend/Advices/ApiError.java
@@ -0,0 +1,20 @@
+package com.nishintgoyal.UberXBackend.Advices;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+public class ApiError
+{
+private HttpStatus status;
+private String message;
+private List<String> subErrors;
+
+}
