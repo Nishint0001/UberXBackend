35c49d7593c0746a116ed295d43cd8649b3baf29
nishint0001 <nishintgoyal0001@gmail.com>
2025-03-19 17:48:29 +0530

    Initial setup UberXBackend

---
+++ src/main/java/com/nishintgoyal/UberXBackend/UberXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nishintgoyal.UberXBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberXBackendApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(UberXBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nishintgoyal/UberXBackend/UberXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishintgoyal.UberXBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberXBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
